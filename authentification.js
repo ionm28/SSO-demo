96288c822b3989507bf46abf5fce4b1a3225ecdb
ionm28 <ion.mihaila03@gmail.com>
2024-04-22 18:36:06 +0200

    serialize+ deserialize

--- authentification.js
+++ authentification.js
@@ -1,18 +1,26 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
   }
-  const passport = require('passport')
-  const GoogleStrategy = require('passport-google-oauth2').Strategy
-  
-  const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
-  const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth2').Strategy
 
-  passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:5000/auth/google/callback",
-  passReqToCallback: true,
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
+
+passport.use(new GoogleStrategy({
+clientID: GOOGLE_CLIENT_ID,
+clientSecret: GOOGLE_CLIENT_SECRET,
+callbackURL: "http://localhost:5000/auth/google/callback",
+passReqToCallback: true,
 },
 function(request, accessToken, refreshToken, profile, done) {
-  return done(null, profile);
+return done(null, profile);
 }))
+
+passport.serializeUser(function(user, done) {
+done(null, user)
+})
+
+passport.deserializeUser(function(user, done) {
+done(null, user)
+})
