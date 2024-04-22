7f93b3dddd73c6d364bce00dd280bedde02a68c6
ionm28 <ion.mihaila03@gmail.com>
2024-04-22 18:34:57 +0200

    implement google strategy

--- authentification.js
+++ authentification.js
@@ -5,4 +5,14 @@ if (process.env.NODE_ENV !== 'production') {
   const GoogleStrategy = require('passport-google-oauth2').Strategy
   
   const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
-  const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRE
+  const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
+
+  passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:5000/auth/google/callback",
+  passReqToCallback: true,
+},
+function(request, accessToken, refreshToken, profile, done) {
+  return done(null, profile);
+}))
