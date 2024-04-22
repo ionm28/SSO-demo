4c75ec102eb835607dffa50a7f1a1e5321a9fb86
ionm28 <ion.mihaila03@gmail.com>
2024-04-22 18:28:20 +0200

    Implementing basic requests

--- server.js
+++ server.js
@@ -6,3 +6,19 @@ const app = express();
 app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
 app.use(passport.initialize())
 app.use(passport.session())
+
+app.get('/', (req, res) => {
+    res.send('<a href="/auth/google">Login with Google</a>')
+  })
+  
+  app.get('/auth/google', isNotLoggedIn,
+    passport.authenticate('google', { scope: ['email', 'profile'] })
+  )
+
+
+  app.get('/auth/google/callback',
+  passport.authenticate('google', {
+    successRedirect: '/protected',
+    failureRedirect: '/auth/google/failure'
+  })
+)
